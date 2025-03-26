4db532f7aaf12674d061ef5804fefa70879222ea
LuizAbudi <luizabudi@hotmail.com>
2025-03-25 23:49:47 -0300

    feat: implement login functionality with AuthHttpService and create LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import Home from './pages/Home';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from './providers/ThemeProvider';
+import { BrowserRouter } from 'react-router-dom';
+import { Suspense } from 'react';
+import LoadingScreen from './components/Loading';
+import AppRouter from './routes';
 
 const queryClient = new QueryClient();
 
@@ -9,10 +12,12 @@ function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Suspense fallback={<LoadingScreen />}>
+            <AppRouter />
+          </Suspense>
+        </BrowserRouter>
         <ReactQueryDevtools initialIsOpen={false} />
-        <div className="flex flex-col items-center justify-center min-h-svh">
-          <Home />
-        </div>
       </QueryClientProvider>
     </ThemeProvider>
   );
