7708c8f189e320dae4c435e579bec0e48544128c
LuizAbudi <luizabudi@hotmail.com>
2025-03-29 03:32:54 -0300

    feat: add ToastContainer for notifications and remove Sonner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ThemeProvider } from './providers/ThemeProvider';
-import { BrowserRouter } from 'react-router-dom';
 import { Suspense } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Bounce, ToastContainer } from 'react-toastify';
+
 import LoadingScreen from './components/Loading';
 import AppRouter from './routes';
+import { ThemeProvider } from './providers/ThemeProvider';
 
 const queryClient = new QueryClient();
 
@@ -14,6 +16,20 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Suspense fallback={<LoadingScreen />}>
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick={false}
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+              limit={2}
+              transition={Bounce}
+            />
             <AppRouter />
           </Suspense>
         </BrowserRouter>
