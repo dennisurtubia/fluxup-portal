52ccce96c7cad1c3fb3cd0f32ad111bf9c47043d
LuizAbudi <luizabudi@hotmail.com>
2025-03-29 18:53:44 -0300

    refactor: remove unnecessary comments

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -5,21 +5,21 @@ import { isTokenValid } from '@/lib/auth';
 const useAuth = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
     const token = localStorage.getItem('token');
-    return isTokenValid(token); // 🔹 Valida o token na inicialização
+    return isTokenValid(token);
   });
 
   useEffect(() => {
     const checkAuth = () => {
       const token = localStorage.getItem('token');
       if (!isTokenValid(token)) {
-        localStorage.removeItem('token'); // 🔹 Remove token expirado
+        localStorage.removeItem('token');
         setIsAuthenticated(false);
       } else {
         setIsAuthenticated(true);
       }
     };
 
-    window.addEventListener('storage', checkAuth); // Atualiza quando o token muda
+    window.addEventListener('storage', checkAuth);
 
     return () => {
       window.removeEventListener('storage', checkAuth);
