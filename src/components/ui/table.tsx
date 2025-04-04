1a0b166891d1fd62dc6556c3c91329a998d28f3b
LuizAbudi <luizabudi@hotmail.com>
2025-04-03 23:51:05 -0300

    fix: remove wrong property

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -81,7 +81,7 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
     <td
       data-slot="table-cell"
       className={cn(
-        "p-3 px-5 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] break-all max-[300px]",
+        "p-3 px-5 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] break-all",
         className
       )}
       {...props}
