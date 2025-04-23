941faab01be591a477c8a51c44d156fba9fcedde
antoniobt12062002 <antoniotanahashi12@gmail.com>
2025-04-23 00:10:56 -0300

    fix: fix name party

--- src/features/party/pages/PatryPage.tsx
+++ src/features/party/pages/PatryPage.tsx
@@ -10,7 +10,7 @@ import LoadingScreen from '@/components/Loading';
 import { Button } from '@/components/ui/button';
 import Title from '@/components/ui/title';
 
-export default function PartiePage() {
+function PartyPage() {
   const dialogRef = useRef<PartyCreateDialogRef>(null);
 
   const {
@@ -27,7 +27,7 @@ export default function PartiePage() {
     },
   });
 
-  const handleCreatePartie = useCallback(() => {
+  const handleCreateParty = useCallback(() => {
     dialogRef.current?.open();
   }, []);
 
@@ -38,7 +38,7 @@ export default function PartiePage() {
   if (isError) {
     return (
       <div className="flex h-full w-full flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold">Erro ao carregar as tags</h1>
+        <h1 className="text-2xl font-bold">Erro ao carregar os parceiros</h1>
         <p className="mt-4 text-lg">Tente novamente mais tarde.</p>
       </div>
     );
@@ -48,7 +48,7 @@ export default function PartiePage() {
     <div className="h-full w-full">
       <div className="flex justify-between items-center mb-5">
         <Title>Parceiros</Title>
-        <Button variant="default" onClick={handleCreatePartie}>
+        <Button variant="default" onClick={handleCreateParty}>
           Novo Parceiro
         </Button>
 
@@ -59,3 +59,5 @@ export default function PartiePage() {
     </div>
   );
 }
+
+export default PartyPage;
--- src/features/party/routes/index.tsx
+++ src/features/party/routes/index.tsx
@@ -1,11 +1,11 @@
 import { lazy } from 'react';
 import { RouteObject } from 'react-router-dom';
 
-const PartiesPage = lazy(() => import('../pages/PatryPage'));
+const PartyPage = lazy(() => import('../pages/PatryPage'));
 
 export const partiesRoutes: RouteObject[] = [
   {
     path: '/app/parties',
-    element: <PartiesPage />,
+    element: <PartyPage />,
   },
 ];
