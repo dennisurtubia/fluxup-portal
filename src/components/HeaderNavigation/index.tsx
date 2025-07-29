19c97aa3f3839720f02ea094debe35a008bec736
LuizAbudi <luizabudi@hotmail.com>
2025-07-29 00:04:30 -0300

    refactor: remove commented-out code in HeaderNavigation component

--- src/components/HeaderNavigation/index.tsx
+++ src/components/HeaderNavigation/index.tsx
@@ -17,16 +17,6 @@ const HeaderNavigation = ({ title, icon }: HeaderNavigationProps) => {
     navigate(-1);
   }, [navigate]);
 
-  /*
-  <div className="flex items-center mb-5">
-        <Button variant="ghost" className="p-3 mr-3" onClick={handleGoBack}>
-          <ArrowLeft className="size-5 mt-1" />
-        </Button>
-        <Title>Fluxo de caixa</Title>
-      </div>
-  
-  */
-
   return (
     <div className="flex items-center mb-5">
       <Button variant="ghost" className="p-3 mr-3" onClick={handleGoBack}>
