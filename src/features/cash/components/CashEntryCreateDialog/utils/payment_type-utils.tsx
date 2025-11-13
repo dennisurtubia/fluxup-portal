d6d73c25715da532fab4d8cfaca675cd2a9e9a0a
Dennis Urtubia <dennis.urtubia@gmail.com>
2025-11-13 14:20:34 -0300

    feat: adds account credit method

--- src/features/cash/components/CashEntryCreateDialog/utils/payment_type-utils.tsx
+++ src/features/cash/components/CashEntryCreateDialog/utils/payment_type-utils.tsx
@@ -2,12 +2,13 @@ import { PaymentType } from '@/features/cash/http/CashEntryHttpService';
 
 export const paymentTypeLabel: Record<PaymentType, string> = {
   boleto: 'Boleto',
-  pix: 'PIX',
-  ted: 'TED',
   credit_card: 'Cartão de Crédito',
   debit_card: 'Cartão de Débito',
+  account_credit: 'Crédito em conta',
   direct_debit: 'Débito em conta',
   cash: 'Dinheiro',
+  pix: 'PIX',
+  ted: 'TED',
 };
 
 export const paymentTypeOptions = Object.entries(paymentTypeLabel) as [PaymentType, string][];
