7bdd5c47bc1502b883a4fff6ea31da0efe99e4f2
Dennis Urtubia <dennis.urtubia@gmail.com>
2026-07-04 22:44:53 -0300

    feat: enhance button component sizes for better responsiveness on coarse pointer devices

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,10 +22,11 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
+        default:
+          "h-9 px-4 py-2 has-[>svg]:px-3 [@media(pointer:coarse)]:h-11 [@media(pointer:coarse)]:px-5",
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5 [@media(pointer:coarse)]:h-10 [@media(pointer:coarse)]:px-4",
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4 [@media(pointer:coarse)]:h-12",
+        icon: "size-9 [@media(pointer:coarse)]:size-11",
       },
     },
     defaultVariants: {
